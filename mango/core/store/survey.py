e8eed90790f72dcd9af9fa5b73464a27a67a738f
Alireza Hosseini <alireza.hosseini@zoodroom.com>
2019-08-11 07:43:42 +0000

    Get surveys

--- mango/core/store/survey.py
+++ mango/core/store/survey.py
@@ -1,13 +1,16 @@
 from olive.exc import SaveError, CacheNotFound, DocumentNotFound
 from olive.store.cache_wrapper import CacheWrapper
 from mango.core.models.survey import SurveySchema
+from olive.exc import InvalidFilter
+from pprint import pformat
+import pymongo
 
 
 class SurveyStore:
     def __init__(self, db, app):
         self.app = app
         self.db = db
-        self.survey_schema = SurveySchema()
+        self.survey_schema = SurveySchema(exclude_id=True)
         self.cache_key = 'MANGO:SURVEY:{}'
         self.cache_wrapper = CacheWrapper(self.app, self.cache_key)
 
@@ -38,3 +41,24 @@ class SurveyStore:
 
         clean_data = self.survey_schema.load(survey_doc)
         return clean_data
+
+    def get_surveys(self, skip, limit, sort_key='+total_rating'):
+        skip, limit = skip or 0, min(limit or 50, 200)
+        self.app.log.debug('reading surveys from database by below filters...')
+        self.app.log.info('skip={} limit={} sort_key={}'.format(skip, limit, sort_key))
+
+        sort_direction = {
+            '-': pymongo.DESCENDING,
+            '+': pymongo.ASCENDING
+        }
+        if sort_key[:1] not in sort_direction.keys():
+            raise InvalidFilter('invalid sort_key given: {}, it should start with - or +'.format(sort_key))
+
+        survey_docs = self.db.find(filter={},
+                                   projection={'created_at': 0, 'updated_at': 0},
+                                   skip=skip,
+                                   limit=limit,
+                                   sort=[(sort_key[1:], sort_direction[sort_key[:1]])])
+        clean_data = self.survey_schema.load(survey_docs, many=True)
+        self.app.log.info('surveys result: \n{}'.format(pformat(clean_data)))
+        return survey_docs.count(), clean_data
