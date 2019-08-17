de929be1a156dc9c836b659bdf2dff20b55ee1fa
Ali <alireza.hosseini@zoodroom.com>
2019-08-17 12:49:27 +0430

    Add MONGO health check to docker-entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -11,6 +11,15 @@ else
     exit 1
 fi
 
-echo "Starting Mango ..."
+if ! [[ -z "$MONGO_HOST" ]] && ! [[ -z "$MONGO_PORT" ]]
+then
+    until nc -z ${MONGO_HOST} ${MONGO_PORT}; do
+        echo "$(date) - waiting for mongoDB..."
+        sleep 1
+    done
+else
+    echo "\$MONGO_HOST & \$MONGO_PORT is not set"
+    exit 1
+fi
 
 exec "$@"
